1b2a9f710eed4bf9a5c341ab0d438581b56c98b5
sicongkuang <kscnana@gmail.com>
2015-12-12 23:29:30 -0500

    generate feature 31 bias lexicon

--- fea_f31_Nov25.py
+++ fea_f31_Nov25.py
@@ -1,13 +1,13 @@
 import json
 import string
 
-def f31():
+def f31(pathf1f7_,biasfile_,newpath_):
     ## load dataset
-    with open('../../retryData/test_f1f2f3f4f5f6f7_Nov25.json') as f:
+    with open(pathf1f7_) as f:
         tupleSet = json.load(f)
     biasLexicon = []
     ## load biasLexicon
-    with open('/home/sik211/dusk/npov_data/bias-lexicon/bias-lexicon.txt') as j:
+    with open(biasfile_) as j:
         for li in j:
             li = li.rstrip('\n')
             biasLexicon.append(li)
@@ -18,8 +18,8 @@ def f31():
         else:
             tupl['Bias lexicon'] = False
 
-    with open('../../retryData/test_f1f2f3f4f5f6f7f31_Nov26.json','w') as t:
+    with open(newpath_,'w') as t:
         json.dump(tupleSet,t)
 
-f31()
+f31('../../devDataclean_Dec8_2015/dev_f1f2f3f4f5f6f7_stripPuncNum_Dec12Ver2.json','/home/sik211/dusk/npov_data/bias-lexicon/bias-lexicon.txt','../../devDataclean_Dec8_2015/dev_f1f2f3f4f5f6f7f31_stripPuncNum_Dec12.json')
     
