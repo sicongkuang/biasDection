b1c84b95ffe6caddebb8ce9974bbb05d2d525a44
sicongkuang <kscnana@gmail.com>
2015-12-12 23:07:29 -0500

    not in use due to different split up between dependency and corenlp

--- fea_f30_Nov25.py
+++ fea_f30_Nov25.py
@@ -4,7 +4,7 @@ import StanfordDependencies
 from nltk.parse import stanford
 import json
 from corenlp import StanfordCoreNLP
-def f30():
+def f30(file_,wfile_):
     ## stanfordDependencies setting
     sd = StanfordDependencies.get_instance(backend="subprocess",version='3.4.1')
     os.environ['STANFORD_PARSER'] = 'stanford-parser-full-2014-08-27/'
@@ -12,7 +12,7 @@ def f30():
     parser = stanford.StanfordParser(model_path="stanford-parser-full-2014-08-27/stanford-parser-3.4.1-models/edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
     resDepent = []
     ## loaddataset
-    with open('../../dataclean_Nov8_2015/test_afterdataclean_modifiedcleanedTupleNov8.json') as t: 
+    with open(file_) as t: 
         dataset = json.load(t)
 
     for num, tup in enumerate(dataset):
@@ -37,9 +37,9 @@ def f30():
             print detemp
             resDepent.append(detemp)
 
-    with open('../../retryData/test_f30_wPunNum_Nov25.json','w') as u:
+    with open(wfile_,'w') as u:
         json.dump(resDepent,u)
-    len(resDepent)
+    print len(resDepent)
     # with open('../../retryData/test_f1f2f3f4f5f6f7_Nov25.json') as o:
     #     temp = json.load(o)
     # for i,v in zip(temp,detemp[:3]):
@@ -50,5 +50,16 @@ def f30():
     #         print v
     #         print "oh no.."
     #         sys.exit("not match in words split of sentence")
-        
-f30()
+
+def checkCorenlpsplit(file1_,file2_):
+    with open(file1_) as f:
+        data1 = json.load(f)
+    with open(file2_) as t:
+        data2 = json.load(t)
+    for i,j in zip(data1,data2):
+        if i['Word'] != j['Word']:
+            print i['Word']
+            print j['Word']
+
+# f30('../../devDataclean_Dec8_2015/dev_afterdataclean_modifiedcleanedTuple_elimBiasWord0orMoreThanOne_fullTup_Dec11.json','../../devDataclean_Dec8_2015/dev_f30_Dec11.json')
+checkCorenlpsplit('../../devDataclean_Dec8_2015/dev_f30_Dec11.json','../../devDataclean_Dec8_2015/dev_f1f2f3f4f5f6f7_Dec11.json')
